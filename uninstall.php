ab10bb21a8065d9a3322bfe519d295cd3710c3b6
Eric King <eric.king@lonelyplanet.com>
2014-09-13 20:36:01 -0500

    Updated uninstall script

--- uninstall.php
+++ uninstall.php
@@ -3,3 +3,16 @@
 global $wpdb;
 $wpdb->query("DROP TABLE {$wpdb->prefix}http_api_debug_log_headers");
 $wpdb->query("DROP TABLE {$wpdb->prefix}http_api_debug_log");
+
+$options_to_delete = array(
+    'http-api-debug-logs-to-keep',
+    'http-api-debug-purge-after',
+    'http-api-debug-require-wp-debug',
+    'http-api-debug-ignore-cron',
+    'http-api-debug-domain-filter',
+    'http-api-debug-domains'
+);
+
+foreach ($options_to_delete as &$option) {
+    delete_site_option( $option );
+}
